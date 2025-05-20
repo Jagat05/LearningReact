43094ffc9e4305a772af5c9c044e9e5e2ab3280f
Jagat05 <joshijagaths@gmail.com>
2025-05-20 15:55:04 +0545

    Mastering React Day 1 Counter

--- ReactDay1/src/App.jsx
+++ ReactDay1/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [value, setValue] = useState(0);
+
+  const increment = () => {
+    setValue(value + 1);
+    console.log("Value =", value + 1);
+  };
+
+  const decrement = () => {
+    if (value > 0) {
+      setValue(value - 1);
+      console.log("Value =", value - 1);
+    } else {
+      console.log("Value cannot be less than 0");
+    }
+  };
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>Counter</div>
+        <p> Value : {value}</p>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
